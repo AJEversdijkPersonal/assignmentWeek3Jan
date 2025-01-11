f55f480c8419417d4af434949e2ea2c0d80d1a6e
AJEversdijk <ajeversdijk@gmail.com>
2025-01-11 19:25:27 +0100

    JanWeek3: fix a test with a required input

--- src/app/components/transaction-card/transaction-card.component.spec.ts
+++ src/app/components/transaction-card/transaction-card.component.spec.ts
@@ -5,15 +5,27 @@ import { TransactionCardComponent } from './transaction-card.component';
 describe('TransactionCardComponent', () => {
   let component: TransactionCardComponent;
   let fixture: ComponentFixture<TransactionCardComponent>;
+  const mockTransaction = {
+    id: 1,
+    timestamp: '2022-11-08T10:30:47.123456',
+    amount: 17.95,
+    currencyCode: 'USD',
+    currencyRate: 1.173628,
+    description: 'Some interesting description',
+    otherParty: {
+      name: 'Mister X',
+      iban: 'NL00RABO0123456789',
+    },
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [TransactionCardComponent]
-    })
-    .compileComponents();
+      imports: [TransactionCardComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(TransactionCardComponent);
     component = fixture.componentInstance;
+    fixture.componentRef.setInput('transaction', mockTransaction);
     fixture.detectChanges();
   });
 
