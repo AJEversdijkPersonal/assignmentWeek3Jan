59e42f985a2ea23487ac2cc93d0333a5e5cc4e76
AJEversdijk <ajeversdijk@gmail.com>
2025-01-11 20:00:46 +0100

    JanWeek3: init detail page

---
+++ src/app/components/transaction-detail-page/transaction-detail-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transaction-detail-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './transaction-detail-page.component.html',
+  styleUrl: './transaction-detail-page.component.scss'
+})
+export class TransactionDetailPageComponent {
+
+}
