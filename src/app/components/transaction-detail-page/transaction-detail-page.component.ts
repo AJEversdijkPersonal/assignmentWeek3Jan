81b6d28a02c95ba2a679d44097f01354a5d547d5
AJEversdijk <ajeversdijk@gmail.com>
2025-01-11 20:11:12 +0100

    JanWeek3: added the details for the detailPage

--- src/app/components/transaction-detail-page/transaction-detail-page.component.ts
+++ src/app/components/transaction-detail-page/transaction-detail-page.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, computed, input, Signal } from '@angular/core';
+import { Transaction } from '../../model/transactions.model';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-transaction-detail-page',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './transaction-detail-page.component.html',
-  styleUrl: './transaction-detail-page.component.scss'
+  styleUrl: './transaction-detail-page.component.scss',
 })
 export class TransactionDetailPageComponent {
+  transaction = input.required<Transaction>();
 
+  amount: Signal<number> = computed(() => {
+    // I think this syntax is so stupid, but angular wants it this way for now.
+    // I hope they change it eventually.
+    const currencyRate = this.transaction().currencyRate;
+    if (currencyRate) {
+      return this.transaction().amount * currencyRate;
+    }
+    return this.transaction().amount;
+  });
 }
