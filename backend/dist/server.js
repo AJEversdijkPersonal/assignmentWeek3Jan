81a144b7b143971dbd2998d2729008ba994568e7
AJEversdijk <ajeversdijk@gmail.com>
2025-01-12 21:44:57 +0100

    JanWeek3: edited the backend to allow all domains, it was giving a CORS error on the same domain for some reason.

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -3,6 +3,12 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: false })); // support encoded bodies
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Had to edit the backend I was getting CORS errors while on the same domain.
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 app.get('/', (req, res) => {
     console.log(req.body);
     console.log('status success');
