255cf43d4cdb46693c196be40e7d03d4ff02e02b
AJEversdijk <ajeversdijk@gmail.com>
2025-01-12 15:15:21 +0100

    JanWeek3: added some tests

--- src/app/components/main-page/main-page.component.spec.ts
+++ src/app/components/main-page/main-page.component.spec.ts
@@ -1,23 +1,32 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { MainPageComponent } from './main-page.component';
+import { Router } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 describe('MainPageComponent', () => {
   let component: MainPageComponent;
   let fixture: ComponentFixture<MainPageComponent>;
+  let router: Router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MainPageComponent]
-    })
-    .compileComponents();
+      imports: [MainPageComponent, MatButtonModule],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(MainPageComponent);
     component = fixture.componentInstance;
+    router = TestBed.inject(Router);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should navigate to the specified route', () => {
+    const navigateSpy = jest.spyOn(router, 'navigate');
+    const route = '/test-route';
+    component.navigateTo(route);
+    expect(navigateSpy).toHaveBeenCalledWith([route]);
   });
 });
