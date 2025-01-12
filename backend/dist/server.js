124b1c6a5aa76d85acdbb3fda0f145d7e892b345
AJEversdijk <ajeversdijk@gmail.com>
2025-01-12 21:28:46 +0100

    JanWeek3: did a lot of the setup for the api call, WIP still getting a CORS error that needs solving.

---
+++ backend/dist/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: false })); // support encoded bodies
+app.get('/', (req, res) => {
+    console.log(req.body);
+    console.log('status success');
+    res.send('OK');
+});
+app.get('/api/transactions', (req, res) => {
+    res.send(require('./transactions.json'));
+});
+app.listen(8080, () => {
+    console.log('Express app listening on port 8080!');
+});
+//# sourceMappingURL=server.js.map
--- src/app/components/transactions-timeline-page/transactions-timeline-page.component.ts
+++ src/app/components/transactions-timeline-page/transactions-timeline-page.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 import { TransactionsCardListComponent } from '../transactions-card-list/transactions-card-list.component';
+import { TransactionsService } from '../../services/transactions.service';
 
 @Component({
   selector: 'app-transactions-timeline-page',
@@ -8,4 +9,20 @@ import { TransactionsCardListComponent } from '../transactions-card-list/transac
   templateUrl: './transactions-timeline-page.component.html',
   styleUrl: './transactions-timeline-page.component.scss',
 })
-export class TransactionsTimelinePageComponent {}
+export class TransactionsTimelinePageComponent implements OnInit {
+  private transactionService = inject(TransactionsService);
+  private destroyRef = inject(DestroyRef);
+  isFetching = signal(false);
+
+  ngOnInit(): void {
+    const transactionsSub = this.transactionService
+      .getTransactions()
+      .subscribe({
+        complete: () => {
+          this.isFetching.set(false);
+        },
+        error: (error) => console.log(error),
+      });
+    this.destroyRef.onDestroy(() => transactionsSub.unsubscribe());
+  }
+}
