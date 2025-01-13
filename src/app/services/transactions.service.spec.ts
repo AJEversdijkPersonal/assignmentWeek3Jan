c705c07f4d107b5f6db8d620b6736cfb8ee34f1e
AJEversdijk <ajeversdijk@gmail.com>
2025-01-13 22:00:03 +0100

    JanWeek3: was missing a provider for these tests

--- src/app/services/transactions.service.spec.ts
+++ src/app/services/transactions.service.spec.ts
@@ -1,12 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
 import { TransactionsService } from './transactions.service';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('TransactionsService', () => {
   let service: TransactionsService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({ providers: [provideHttpClient()] });
     service = TestBed.inject(TransactionsService);
   });
 
