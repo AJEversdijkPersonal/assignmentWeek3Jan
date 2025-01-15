a230246e373fb8407b8f2360aa92db2fa16f5ca9
AJEversdijk <ajeversdijk@gmail.com>
2025-01-15 22:10:45 +0100

    JanWeek3: sorting the array because it was in the requirements.

--- src/app/components/transactions-card-list/transactions-card-list.component.ts
+++ src/app/components/transactions-card-list/transactions-card-list.component.ts
@@ -1,8 +1,16 @@
-import { Component, inject, NgZone, Signal } from '@angular/core';
+import {
+  Component,
+  computed,
+  inject,
+  NgZone,
+  signal,
+  Signal,
+} from '@angular/core';
 import { MatListModule } from '@angular/material/list';
 import { TransactionCardComponent } from '../transaction-card/transaction-card.component';
 import { Router } from '@angular/router';
 import { TransactionsService } from '../../services/transactions.service';
+import { Transactions } from '../../model/transactions.model';
 9;
 @Component({
   selector: 'app-transactions-card-list',
@@ -15,10 +23,31 @@ export class TransactionsCardListComponent {
   private transactionsService = inject(TransactionsService);
   transactions = this.transactionsService.loadedTransactions;
   constructor(private router: Router) {}
+  asc = signal(true);
+  computedDays = computed(() => {
+    console.log(this.transactions);
+    const transactions = this.transactions();
+    const sortedTransactionsByDays = transactions
+      ? this.sortArrayByDate(transactions, this.asc())
+      : transactions;
+    return sortedTransactionsByDays;
+  });
 
   onCardClick = (dayId: string, transactionId: number) => {
     this.router.navigate(['/transaction'], {
       queryParams: { dayId: dayId, transactionId: transactionId },
     });
+  };
+
+  sortArrayByDate = (arr: Transactions, asc: boolean) => {
+    // TODO if you are seeing this text I did not have enough time to implement the asc/desc logic
+    if (arr.days.length === 0) return;
+    const newArr = arr.days.sort(function (a, b) {
+      return Number(new Date(b.id)) - Number(new Date(a.id));
+    });
+    if (!asc) {
+      return newArr.reverse();
+    }
+    return newArr;
   };
 }
